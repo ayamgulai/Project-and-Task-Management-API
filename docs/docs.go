e79cfb2bb9e36385c03805f57f71c981c54db552
samuel <samuelhutagalung00@gmail.com>
2026-02-21 00:45:57 +0700

    fix: host from local to hosted address

--- docs/docs.go
+++ docs/docs.go
@@ -785,7 +785,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "project-and-task-management-api-production.up.railway.app/",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Mini Jira Backend",
